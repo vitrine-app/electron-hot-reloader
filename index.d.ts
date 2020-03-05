15efd25b3010b6a093fbd5b99588ac15009a6429
Paul Roman <roman.paul.pro@gmail.com>
2020-03-05 15:17:16 +0100

    feat: Use webpack exclusively

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,9 @@
 export type Options = {
-  electronPath?: string;
-  hardResetMethod?: 'quit' | 'exit';
   argv?: string[];
-  appDirectory?: string;
-  watchRenderer?: boolean;
   ignored?: (string | RegExp)[];
   debug?: boolean;
 };
 
-declare function reload(moduleObject: NodeModule, options?: Options): void;
+declare function reload(watchedPath: string, appDirectory: string, electronPath: string, options?: Options): void;
 
 export default reload;
--- src/main.js
+++ src/main.js
@@ -1,118 +1,75 @@
-import path from 'path';
 import fs from 'fs';
 import { spawn } from 'child_process';
 import { inspect } from 'util';
 
-
-import findUp from 'find-up';
 import chalk from 'chalk';
 import dateTime from 'date-time';
 import isDev from 'electron-is-dev';
 import chokidar from 'chokidar';
-import { app, BrowserWindow } from 'electron';
-
-const getMainProcessPaths = (topModuleObject, cwd) => {
-  const paths = new Set([topModuleObject.filename]);
-
-  const getPaths = moduleObject => {
-    for (const child of moduleObject.children) {
-      if (path.relative(cwd, child.filename).includes('node_modules')) {
-        continue;
-      }
-
-      paths.add(child.filename);
-      getPaths(child);
-    }
-  };
-
-  getPaths(topModuleObject);
-
-  return paths;
-}
+import { app } from 'electron';
 
 /**
  *
- * @param moduleObject {NodeModule} - The required module object
- * @param options.electronPath {string?} - Electron binary path
+ * @param watchedPath {string} - The directory of watched files
+ * @param appDirectory {string} - App directory used for hard reset
+ * @param electronPath {string} - Electron binary path
  * @param options {Object?} - Options
- * @param options.hardResetMethod {'quit' | 'exit'?} - Gracefully exit or not
  * @param options.argv {string[]?} - Gracefully exit or not
- * @param options.appDirectory {string?} - App directory used for hard reset
- * @param options.watchRenderer {boolean?} - True if rendered process should be watched as well
  * @param options.ignored {(string | RegExp)[]?} - Ignored globs
  * @param options.debug {boolean?} - True to display debug logs
  */
-const reload = (moduleObject, options = {}) => {
+const reload = (watchedPath, appDirectory, electronPath, options = {}) => {
   /* This module should be a dev dependency, but this in case the user included it as a dependency. */
   if (!isDev) {
     return;
   }
 
-  if (!moduleObject) {
-    throw new Error('You have to pass the `module` object');
+  if (!watchedPath) {
+    throw new Error('You have to pass the `watchedPath` string');
   }
 
-  const formattedOptions = {
-    watchRenderer: true,
-    ...options,
-  };
-  const { electronPath, appDirectory } = formattedOptions;
-  if (electronPath && !appDirectory) {
-    throw new Error('You cannot hot reload your Electron binary without specifying your application directory.')
+  if (!electronPath || !appDirectory) {
+    throw new Error('You cannot hot reload your Electron binary without specifying your application directory or binary.')
   }
-
-  const mainProcessDirectory = path.dirname(moduleObject.filename);
-  const packageDirectory = findUp.sync('package.json', { cwd: mainProcessDirectory });
-  const cwd = packageDirectory ? path.dirname(packageDirectory) : mainProcessDirectory;
-  const mainProcessPaths = getMainProcessPaths(moduleObject, cwd);
-  const watchPaths = formattedOptions.watchRenderer ? cwd : [...mainProcessPaths];
   let isRelaunching = false;
 
-  const watcher = chokidar.watch(watchPaths, {
-    cwd,
+  /* For the moment, this module will restart even if a change is made on the renderer process. That will change in the future */
+  const watcher = chokidar.watch([watchedPath], {
+    cwd: appDirectory,
     disableGlobbing: true,
     ignored: [
       /(^|[/\\])\../, // Dotfiles
       'node_modules',
       '**/*.map',
-      ...(formattedOptions.ignored || []),
+      ...(options.ignored || []),
     ],
   });
 
-  if (formattedOptions.debug) {
+  if (options.debug) {
     watcher.on('ready', () => {
       console.log('Watched paths:', inspect(watcher.getWatched(), { compact: false, colors: true }));
     });
   }
 
   watcher.on('change', filePath => {
-    if (formattedOptions.debug) {
+    if (options.debug) {
       console.log('File changed:', chalk.bold(filePath), chalk.dim(`(${dateTime().split(' ')[1]})`));
     }
-
-    if (mainProcessPaths.has(path.join(cwd, filePath))) {
-      // Prevent multiple instances of Electron from being started due to the change
-      // handler being called multiple times before the original instance exits.
-      if (!isRelaunching) {
-        if (electronPath && fs.existsSync(electronPath)) {
-          const args = [...(formattedOptions.argv || []), formattedOptions.appDirectory];
-          const child = spawn(electronPath, args, {
-            detached: true,
-            stdio: 'inherit'
-          })
-          child.unref()
-        } else {
-          app.relaunch();
-        }
-        app.exit(0);
+    if (!isRelaunching) {
+      if (electronPath && fs.existsSync(electronPath)) {
+        const args = [...(options.argv || []), appDirectory];
+        const child = spawn(electronPath, args, {
+          detached: true,
+          stdio: 'inherit'
+        })
+        child.unref()
+      } else {
+        app.relaunch();
       }
-
-      isRelaunching = true;
-    } else {
-      BrowserWindow.getAllWindows().forEach(browserWindow => {
-        browserWindow.webContents.reloadIgnoringCache();
-      });
+      app.exit(0);
     }
+
+    isRelaunching = true;
   });
 };
 
